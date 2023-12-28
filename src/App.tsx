f9d6245509dba8b81df5babb0f2a3bc4894a26d1
George Black <contact@george.black>
2023-12-28 22:21:25 +0000

    Init front-end only project for v1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <input
+        className="input"
+        type="text"
+        value={"description"}
+        onChange={(e) => {}}
+        autoFocus={true}
+      ></input>
+    </div>
+  );
+}
+
+export default App;
