75b26b04f27268268177fcdfb2613d5f740ae5e2
George Black <contact@george.black>
2023-12-30 04:05:50 +0000

    Start on new components for auto-filling

---
+++ src/components/Autosuggest.tsx
@@ -0,0 +1,13 @@
+function Autosuggest({
+  value,
+  suggestions,
+  onChange,
+}: {
+  value: string;
+  suggestions: string[];
+  onChange: (value: string) => void;
+}) {
+  return <h1>hey</h1>;
+}
+
+export default Autosuggest;
