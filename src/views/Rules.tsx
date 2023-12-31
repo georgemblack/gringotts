2ed99029a7649c4ceeb32107fd5cd8d902c7a698
George Black <contact@george.black>
2023-12-31 23:09:20 +0000

    Remove unneeded form on rules page

--- src/views/Rules.tsx
+++ src/views/Rules.tsx
@@ -1,27 +1,10 @@
 import { useLiveQuery } from "dexie-react-hooks";
-import { useState } from "react";
 
 import { db } from "../lib/DB";
-import { deleteRule, saveRule } from "../lib/Repository";
+import { deleteRule } from "../lib/Repository";
 
 function Rules() {
   const rules = useLiveQuery(() => db.rules.toArray()) || [];
-
-  const [merchant, setMerchant] = useState("");
-  const [merchantCategory, setMerchantCategory] = useState("");
-  const [category, setCategory] = useState("");
-
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    await saveRule({
-      merchant,
-      merchantCategory,
-      category,
-    });
-    setMerchant("");
-    setMerchantCategory("");
-    setCategory("");
-  };
 
   const handleDelete = async (id: number) => {
     await deleteRule(id);
@@ -58,43 +41,6 @@ function Rules() {
           ))}
         </tbody>
       </table>
-      <div className="card">
-        <div className="card-content">
-          <div className="content">
-            <form onSubmit={handleSubmit}>
-              <div className="flex gap-2">
-                <div className="grow">
-                  <input
-                    className="input"
-                    placeholder="Merchant"
-                    value={merchant}
-                    onChange={(e) => setMerchant(e.target.value)}
-                  />
-                </div>
-                <div className="grow">
-                  <input
-                    className="input"
-                    placeholder="Merchant Category"
-                    value={merchantCategory}
-                    onChange={(e) => setMerchantCategory(e.target.value)}
-                  />
-                </div>
-                <div className="grow">
-                  <input
-                    className="input"
-                    placeholder="Category"
-                    value={category}
-                    onChange={(e) => setCategory(e.target.value)}
-                  />
-                </div>
-                <button type="submit" className="button is-primary">
-                  Save
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
