a1fb5e3e2655946795acc35dc860a2e229f7dde2
George Black <contact@george.black>
2024-01-01 18:43:07 +0000

    Start summary view

--- src/views/Summary.tsx
+++ src/views/Summary.tsx
@@ -1,4 +1,17 @@
+import { Month } from "../lib/Types";
+
 function Summary() {
-  return <p>Summary</p>;
+  return (
+    <table className="table mt-4 w-full">
+      <thead>
+        <tr>
+          <th></th>
+          {Object.values(Month).map((month) => (
+            <th>{month}</th>
+          ))}
+        </tr>
+      </thead>
+    </table>
+  );
 }
 export default Summary;
